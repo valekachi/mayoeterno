359617310776d1925b5c3a9b009a3e14e6727fd2
Valentina <valentinakachi@gmail.com>
2022-08-21 02:54:49 -0300

    Desafio menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidxs!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar />
+     <main></main>
+     <footer></footer>
+       
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return (
+        <header>
+          <h1 className="titulo">MAYO ETERNO | JABONES ARTESANALES </h1>
+        <nav>
+          <ul className="barraNav">
+            <li>
+              <a href="">INICIO</a>
+              </li>
+            <li>
+            <a href="">CATALOGO</a>
+            </li>
+            <li>
+            <a href="">CONTACTO</a>
+            </li>
+          </ul>
+        </nav>
+       </header>
+    );
+};
+
+export default NavBar;
