8235a75baeada20589ed973e1faaf327c3a12633
Valentina <valentinakachi@gmail.com>
2022-08-24 22:22:46 -0300

    Arreglado error en ItemCount.jsx

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const ItemCount = () => {
-        const [count, setCount] = useState(0);
+        const [count, setCount] = useState(1);
 
         let tope = 15
         const suma = () => {
@@ -13,8 +13,8 @@ const ItemCount = () => {
   return (
     <div>
         <button onClick={suma}>+</button>
-        <button onClick={resta}>-</button>
-        <button disabled={count === 0}>Agregar a mi carrito</button>
+        <button onClick={resta} disabled={count === 1}>-</button>
+        <button>Agregar a mi carrito</button>
         <p>Contador: {count} </p>
 
     </div>
