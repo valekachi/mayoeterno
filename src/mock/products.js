5a55024a91515f3ad61d22d86d6d473e1d1a1a4e
Valentina <valentinakachi@gmail.com>
2022-09-02 00:37:40 -0300

    Mas styla

--- src/mock/products.js
+++ src/mock/products.js
@@ -13,7 +13,7 @@ export const productos = [
         price: 230,
         stock: 15,
         category: "Jabones",
-        img: 'https://cdn.shopify.com/s/files/1/2395/7673/files/soap-recipe-2_480x480.jpg?v=1656498002'
+        img: 'https://t2.uc.ltmcdn.com/es/posts/5/0/3/jabon_de_carbon_activado_para_que_sirve_y_como_hacerlo_50305_600.jpg'
     },
     {
         id: 3,
@@ -21,30 +21,30 @@ export const productos = [
         price: 230,
         stock: 15,
         category: "Jabones",
-        img: 'https://cdn.shopify.com/s/files/1/2395/7673/files/soap-recipe-2_480x480.jpg?v=1656498002'
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKVcIX2f8xqMIT3Kkkr92HZLr--J8FWZEzgA&usqp=CAU'
     },
     {
-    id: 1,
-    title: "Jabón de avena",
+    id: 4,
+    title: "Jabón de lavanda",
     price: 230,
     stock: 15,
     category: "Jabones",
-    img: 'https://cdn.shopify.com/s/files/1/2395/7673/files/soap-recipe-2_480x480.jpg?v=1656498002'
+    img:'https://cdn.bioguia.com/embed/2a0505bdea788472527d6c94930eb1524371119/Como_hacer_jabon_de_lavanda_relajante_para_renovar_tu_energia_y_calmar_tu_mente'
     },
     {
-        id: 1,
-        title: "Jabón de avena",
+        id: 5,
+        title: "Jabón de malva",
         price: 230,
         stock: 15,
         category: "Jabones",
-        img: 'https://cdn.shopify.com/s/files/1/2395/7673/files/soap-recipe-2_480x480.jpg?v=1656498002'
+        img: 'https://media.vogue.mx/photos/5c06fa5e32b1ca0f93f046b0/master/w_1600%2Cc_limit/jabones_artesanales_hechos_orgullosamente_en_mexico_9684.jpg'
     },
     {
-        id: 1,
-        title: "Jabón de avena",
+        id: 6,
+        title: "Jabón de menthol",
         price: 230,
         stock: 15,
         category: "Jabones",
-        img: 'https://cdn.shopify.com/s/files/1/2395/7673/files/soap-recipe-2_480x480.jpg?v=1656498002'
+        img: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/como-hacer-jabon-de-menta-casero-3.jpg'
     }
 ]
